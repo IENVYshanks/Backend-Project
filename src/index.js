3411955a6dc54d4fd684ce85feef51dc5d2c418f
IENVYshanks <sawaiyanronit12@gmail.com>
2026-03-25 19:49:11 +0530

    api responses

--- src/index.js
+++ src/index.js
@@ -9,9 +9,12 @@ dotenv.config({
 
 
 
-connectDB().then(()=>{
-    
-        console.log("Connected")
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`)
+    })
+
 })
 .catch((err) => {
     console.log("error",err)
